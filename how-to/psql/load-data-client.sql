bbbe2fef7b10d3399e9be33b2411a4f6800994b5
copiousfreetime <jeremy@copiousfreetime.org>
2020-08-08 14:20:18 -0600

    update the load instructions for both client and server options
    - distinguish between COPY and \COPY methods
    - fix the copy command to use the appropriate tab character
    - set HEADER to false since the awk command strips off the header.

---
+++ how-to/psql/load-data-client.sql
@@ -0,0 +1,4 @@
+\COPY unsplash_photos      FROM PROGRAM 'awk FNR-1 {path}/photos.tsv* | cat'      WITH ( FORMAT csv, DELIMITER E'\t', HEADER false);
+\COPY unsplash_keywords    FROM PROGRAM 'awk FNR-1 {path}/keywords.tsv* | cat'    WITH ( FORMAT csv, DELIMITER E'\t', HEADER false);
+\COPY unsplash_collections FROM PROGRAM 'awk FNR-1 {path}/collections.tsv* | cat' WITH ( FORMAT csv, DELIMITER E'\t', HEADER false);
+\COPY unsplash_conversions FROM PROGRAM 'awk FNR-1 {path}/conversions.tsv* | cat' WITH ( FORMAT csv, DELIMITER E'\t', HEADER false);
