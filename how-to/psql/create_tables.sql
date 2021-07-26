45021bb0c000b62a64757c0fc3e06deef349e7e4
TimmyCarbone <timothy@timmycarbone.com>
2021-07-26 13:53:25 -0400

    Add blur_hash in how-to SQL import

--- how-to/psql/create_tables.sql
+++ how-to/psql/create_tables.sql
@@ -30,7 +30,8 @@ CREATE TABLE unsplash_photos (
   ai_primary_landmark_name varchar(255),
   ai_primary_landmark_latitude float,
   ai_primary_landmark_longitude float,
-  ai_primary_landmark_confidence varchar(255)
+  ai_primary_landmark_confidence varchar(255),
+  blur_hash varchar(255)
 );
 
 CREATE TABLE unsplash_keywords (
